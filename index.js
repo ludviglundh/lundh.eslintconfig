10e148dd7a1d25a6881f9bd9259b74f3d278de65
Ludvig Lundh <ludvig.lundh@ancon.io>
2020-09-28 09:41:30 +0200

    init

---
+++ index.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: [
+    require.resolve('eslint-config-airbnb'),
+    require.resolve('eslint-config-prettier'),
+    require.resolve('eslint-config-prettier/react'),
+  ],
+  parser: 'babel-eslint',
+  plugins: ['prettier', 'react-hooks'],
+  env: {
+    browser: true,
+  },
+  globals: {
+    __DEV__: 'readonly',
+  },
+  rules: {
+    'no-bitwise': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'import/prefer-default-export': 'off',
+    'comma-dangle': ['error', 'always-multiline'],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/jsx-filename-extension': 'off',
+    'react/sort-comp': 'off',
+  },
+  overrides: [
+    {
+      files: ['*Slice.js'], // Redux-toolkit slices use immer
+      rules: {
+        'no-param-reassign': 0,
+      },
+    },
+  ],
+}
